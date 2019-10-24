3cf466e737edc976f3b7e0666c22d6245ce9a6c2
fabricioeliasbr <fabricio.silva226@etec.sp.gov.br>
2019-10-24 19:36:19 -0300

    Formatei o layout de cadastro, adaptando as inputs aos respectivos tipos de dados que irão receber, e fiz algumas alterações de tamanho dos elementos. Criei a activity de listagem de usuários.

---
+++ app/src/main/java/br/com/etecia/appagenda/ListaActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.appagenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_lista);
+    }
+}
