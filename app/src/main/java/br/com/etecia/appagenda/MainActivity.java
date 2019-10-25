e11d6a11aa7b193a445fa7d6decb41d7463885d2
fabricioeliasbr <fabricio.silva226@etec.sp.gov.br>
2019-10-24 22:05:11 -0300

    Criei os métodos principais da MainActivity.

--- app/src/main/java/br/com/etecia/appagenda/MainActivity.java
+++ app/src/main/java/br/com/etecia/appagenda/MainActivity.java
@@ -3,12 +3,31 @@ package br.com.etecia.appagenda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSair, btnCad, btnLista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Inflater inflater = this.btnCad
+        if (btnCad.isEnabled()) {
+            cadastraUser();
+        }
     }
+
+    private void cadastraUser() {}
+
+    public void sairSistema(View view) {
+        if (btnSair.isEnabled()) {
+            System.exit(0);
+        }
+    }
+
 }
